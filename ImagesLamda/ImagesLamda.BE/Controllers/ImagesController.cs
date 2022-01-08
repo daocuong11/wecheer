936c63c4c4dd9ea34075fa1cbed194d3b81da997
cuong lam <none>
2022-01-08 15:18:52 +0700

    add BE code

---
+++ ImagesLamda/ImagesLamda.BE/Controllers/ImagesController.cs
@@ -0,0 +1,44 @@
+﻿using ImagesLamda.BE.Models;
+using ImagesLamda.BE.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImagesLamda.BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly ImageService imageService;
+
+        public ImagesController(ImageService imageService)
+        {
+            this.imageService = imageService;
+        }
+
+        [HttpGet]
+        [Route("GetLastImage")]
+        public ActionResult GetLastImage()
+        {
+            return Ok(imageService.GetLastImage());
+        }
+
+        [HttpGet]
+        [Route("GetImageInHour")]
+        public ActionResult GetImageInHour()
+        {
+            return Ok(imageService.GetImageInHour());
+        }
+
+        [HttpPost]
+        public ActionResult ÂddImage([FromBody] Image img)
+        {
+            imageService.Add(img);
+            return new ObjectResult(img) { StatusCode = StatusCodes.Status201Created };
+        }
+    }
+}
---
+++ ImagesLamda/ImagesLamda.BE/Services/ImageService.cs
@@ -0,0 +1,34 @@
+﻿using ImagesLamda.BE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImagesLamda.BE.Services
+{
+    public class ImageService
+    {
+        private List<Image> images;
+
+        public ImageService()
+        {
+            images = new List<Image>();
+        }
+
+        public void Add(Image img)
+        {
+            img.AddedTime = DateTime.Now;
+            this.images.Add(img);
+        }
+
+        public int GetImageInHour()
+        {
+            return this.images.Where(e => (DateTime.Now - e.AddedTime).TotalMinutes <= 60).Count();
+        }
+
+        public Image GetLastImage()
+        {
+            return this.images.LastOrDefault();
+        }
+    }
+}
